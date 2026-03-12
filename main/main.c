cd46e4d30d6bb485f748c9e29e620edb1222aed5
Elyas Bianchi <elyasb99@gmail.com>
2026-03-11 23:31:10 -0700

    rename some stuff

--- main/main.c
+++ main/main.c
@@ -1,31 +1,26 @@
-#include "driver/i2c_types.h"
-#include "driver/spi_master.h"
-#include "i2c/i2c_helpers.h"
+#include "i2c/i2c.h"
 #include "lora/lora.h"
 #include "oled/oled.h"
 #include <driver/gpio.h>
+#include <driver/i2c_types.h>
+#include <driver/spi_master.h>
 #include <stdint.h>
 #include <stdio.h>
 
-#include "i2c/i2c_helpers.c"
+#include "i2c/i2c.c"
 #include "lora/lora.c"
 #include "oled/oled.c"
 
 void app_main() {
-  // init OLED
   i2c_master_dev_handle_t dev_handle = i2c_init();
   oled_init(dev_handle);
 
-  // init LoRa
   spi_device_handle_t lora_handle = lora_init();
 
-  // read frequency
   uint32_t frf = lora_get_freq(lora_handle);
 
-  // convert to MHz as string
   char freq_str[16]; // buffer for string
   snprintf(freq_str, sizeof(freq_str), "%.3f MHz", (double)frf);
 
-  // draw on OLED
   oled_draw_string(dev_handle, freq_str, 0, 7);
 }
