c237b0f0dc207fa3534fac66b8ef5311b70c846b
Elyas Bianchi <elyasb99@gmail.com>
2026-03-26 16:19:46 -0700

    delete more uart

--- main/main.c
+++ main/main.c
@@ -44,16 +44,6 @@ static void oled_task(void *arg) {
 
 void app_main() {
     printf("\n");
-    uart_config_t uart_config = {
-        .baud_rate = 115200,
-        .data_bits = UART_DATA_8_BITS,
-        .parity = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-        .source_clk = UART_SCLK_APB,
-    };
-    uart_param_config(UART_PORT, &uart_config);
-    uart_driver_install(UART_PORT, 256, 0, 0, NULL, 0);
 
     i2c_master_dev_handle_t i2c_handle = i2c_init();
     oled_state_t oled = oled_init(i2c_handle);
